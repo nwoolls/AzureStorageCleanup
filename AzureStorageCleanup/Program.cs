3b72fdca3ef5c3d7a6fcff6ca45a4138c9eb6838
Nate Woolls <nwoolls@gmail.com>
2013-07-09 22:39:02 -0400

    Companion to SQLDatabaseBackup, clean up Azure Storage files

---
+++ AzureStorageCleanup/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AzureStorageCleanup
+{
+    class Program
+    {
+        static class CommandLineArgument
+        {
+            internal const string StorageName = "-storagename";
+            internal const string StorageKey = "-storagekey";
+            internal const string Container = "-container";
+            internal const string MinDaysOld = "-mindaysold";
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                WriteHelpToConsole();
+                return;
+            }
+
+            if (!CheckArgumentsExist(CommandLineArgument.StorageName, CommandLineArgument.StorageKey, CommandLineArgument.Container, CommandLineArgument.MinDaysOld))
+                return;
+            
+            string storageAccountName = GetArgumentValue(CommandLineArgument.StorageName);  // storage account
+            string storageAccessKey = GetArgumentValue(CommandLineArgument.StorageKey);     // storage key
+            string containerName = GetArgumentValue(CommandLineArgument.Container);         // storage container
+
+            bool success = false;
+            int minDaysOld = -1;
+            success = int.TryParse(GetArgumentValue(CommandLineArgument.MinDaysOld), out minDaysOld);
+
+            if (!success || (minDaysOld < 0))
+            {
+                Console.WriteLine(string.Format("Please supply a valid \"{0}\" argument", CommandLineArgument.MinDaysOld));
+                return;
+            }
+            
+            StorageCleaner storageCleaner = new StorageCleaner(storageAccountName, storageAccessKey, containerName, minDaysOld);
+            storageCleaner.Cleanup();
+        }
+
+        private static void WriteHelpToConsole()
+        {
+            Console.WriteLine(@"
+    Windows Azure Storage Cleanup Utility
+
+    This utility will delete the blob files within the specified Azure Storage container older than the specified number of days.
+
+    Please supply for following command line arguments:
+
+        -storagename [Blob Storage account name]
+        -storagekey  [Blob Storage account key]
+        -container   [Blob Storage container to use]
+        -mindaysold  [Minimum age of files to delete]
+
+    Example usage:
+
+    AzureStorageCleanup.exe
+        -storagename storageaccount
+        -storagekey dmASdd1mg/qPeOgGmCkO333L26cNcnUA1uMcSSOFM...
+        -container sqlbackup
+        -mindaysold 60
+    ");
+        }
+
+        public static string GetArgumentValue(string argumentName)
+        {
+            if (string.IsNullOrEmpty(argumentName)) throw new ArgumentNullException("argumentName");
+
+            string[] arguments = Environment.GetCommandLineArgs();
+            List<string> lowerArguments = new List<string>(arguments.Select(x => x.ToLower()));
+            int index = lowerArguments.IndexOf(argumentName.ToLower());
+
+            if (index == -1)
+                return null;
+
+            if (arguments.Length < index + 2)
+                return null;
+
+            return arguments[index + 1];
+        }
+
+        public static bool CheckArgumentsExist(params string[] argumentNames)
+        {
+            bool returnVal = true;
+
+            foreach (string arg in argumentNames)
+            {
+                if (GetArgumentValue(arg) == null)
+                {
+                    Console.WriteLine(String.Format("Please supply the \"{0}\" argument", arg));
+                    returnVal = false;
+                }
+            }
+
+            return returnVal;
+        }
+    }
+}
---
+++ AzureStorageCleanup/StorageCleaner.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Linq;
+
+namespace AzureStorageCleanup
+{
+    class StorageCleaner
+    {
+        private readonly string storageAccountName;
+        private readonly string storageAccessKey;
+        private readonly string containerName;
+        private readonly int minDaysOld;
+
+        public StorageCleaner(string storageAccountName, string storageAccessKey, string containerName, int minDaysOld)
+        {
+            this.storageAccountName = storageAccountName;
+            this.storageAccessKey = storageAccessKey;
+            this.containerName = containerName;
+            this.minDaysOld = minDaysOld;
+        }
+
+        public void Cleanup()
+        {            
+            StorageCredentials storageCredentials = new StorageCredentials(storageAccountName, storageAccessKey);
+            CloudStorageAccount account = new CloudStorageAccount(storageCredentials, true);
+
+            CloudBlobClient blobClient = account.CreateCloudBlobClient();
+
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+
+            Console.WriteLine("Deleting blob storage files in {0}\\{1} older than {2} days", storageAccountName, containerName, minDaysOld);
+
+            DateTime referenceDate = DateTime.UtcNow;
+            var blobQuery = from b in container.ListBlobs().OfType<CloudBlockBlob>()
+                            where b.Properties.LastModified <= referenceDate.AddDays(-minDaysOld)
+                            select b;
+
+            var blobList = blobQuery.ToList();
+
+            if (blobList.Count == 0)
+            {
+                Console.WriteLine("No files found in {0}\\{1} older than {2} days", storageAccountName, containerName, minDaysOld);
+                return;
+            }
+
+            foreach (CloudBlockBlob blob in blobList)
+            {
+                Console.WriteLine("Deleting blob storage file {0}\\{1}, {2} days old", containerName, blob.Name, (referenceDate - blob.Properties.LastModified.Value).TotalDays);
+                blob.DeleteIfExists();
+            }
+
+            Console.WriteLine("{0} blob storage files deleted in {0}\\{1} older than {2} days", storageAccountName, containerName, minDaysOld);
+        }
+    }
+}
